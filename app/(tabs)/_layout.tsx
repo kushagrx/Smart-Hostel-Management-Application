9050e2416e47d0cc9534e2c0314af5940d476d10
kushagrx <kushagrabisht10@gmail.com>
2025-09-22 17:26:19 +0530

    Added on press colors to the icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,58 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
 
 const _layout = () => {
   return (
-    <Tabs>
-        <Tabs.Screen name="index" options={{ headerShown: false, title: 'Home' }} />
-        <Tabs.Screen name="alerts" options={{ headerShown: false, title: 'Alerts' }} />
-        <Tabs.Screen name="profile" options={{ headerShown: false, title: 'Profile' }} />
-        
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: 'orange', 
+      tabBarInactiveTintColor: 'gray',  
+    }}>
+        <Tabs.Screen 
+          name="index" 
+          options={{ 
+            headerShown: false, 
+            title: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <AntDesign name="home" size={size} color={color} />
+            )
+          }} 
+        />
+
+        <Tabs.Screen 
+          name="alerts" 
+          options={{ 
+            headerShown: false, 
+            title: 'Alerts',
+            tabBarIcon: ({color, size}) => (
+              <AntDesign name="bell" size={size} color={color} />
+            )
+          }} 
+        />
+
+        <Tabs.Screen 
+          name="profile" 
+          options={{ 
+            headerShown: false, 
+            title: 'Profile',
+            tabBarIcon: ({color, size}) => (
+              <AntDesign name="user" size={size} color={color} />
+            )
+          }} 
+        />
+
+        <Tabs.Screen 
+          name="settings" 
+          options={{ 
+            headerShown: false, 
+            title: 'Settings',
+            tabBarIcon: ({color, size}) => (
+              <Feather name="settings" size={size} color={color} />
+            )
+          }} 
+        />
     </Tabs>
   )
 }
